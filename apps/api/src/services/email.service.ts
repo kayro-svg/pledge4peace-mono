bf47e2cddf081ab142dc9226d8373778a6603c43
Kayro Valero <sannelvalero@gmail.com>
2025-07-07 00:52:07 -0500

    fix(api): update email recipient in EmailService to use new address for testing; comment out previous email for future reference

--- apps/api/src/services/email.service.ts
+++ apps/api/src/services/email.service.ts
@@ -531,8 +531,9 @@ export class EmailService {
           name: "Pledge4Peace Admin",
         },
         {
-          email: "shelsys@pledge4peace.org",
-          name: "Shelsys Rivera - Marketing Chief",
+          // email: "shelsys@pledge4peace.org",
+          email: "kayrov@weversity.org",
+          name: "Kayro Test - this will be replaced by Shelsys Rivera - Marketing Chief",
         },
       ],
       subject: `New Pledge from ${pledgeData.userName}`,
