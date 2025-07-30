9fc847c9bab914cc6d3415dff26cf6e2b64607be
Kayro Valero <sannelvalero@gmail.com>
2025-07-30 13:04:28 -0500

    refactor(web): comment out Google Translate component in layout for future reference

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -56,7 +56,7 @@ export default function RootLayout({
       <body className={`${inter.className} h-full w-[100%]`}>
         <GoogleAnalytics />
         <FacebookPixel />
-        <GoogleTranslate />
+        {/* <GoogleTranslate /> */}
         <Providers>
           <LayoutWrapper>{children}</LayoutWrapper>
           <SessionExpiryWarning />
